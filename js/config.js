0961fa5bba5063417475952e3be01cb47f22ee26
Saleh Bagomri <s.bagomri@gmail.com>
2026-06-11 02:36:51 +0300

    feat: إضافة رابط LinkedIn الحقيقي

--- js/config.js
+++ js/config.js
@@ -58,7 +58,7 @@ const siteConfig = {
             github: 'https://github.com/salehbagomri',
             behance: 'https://www.behance.net/salehbbagomri',
             instagram: 'https://instagram.com/salehbagomri',
-            linkedin: 'https://linkedin.com/in/yourusername',
+            linkedin: 'https://www.linkedin.com/in/salehbagomri',
             dribbble: 'https://dribbble.com/yourusername'
         }
     },
